ade87f15a1fb5a8a23ed69269db21e42a74e899b
vivekChenna <chennavivek96@gmail.com>
2025-01-13 11:52:34 +0530

    routed to marketplace for get started today button

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -1,15 +1,24 @@
-'use client'
+"use client";
 
-import Link from 'next/link'
-import { Button } from "@/components/ui/button"
-import Image from 'next/image'
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
 
 export default function Header() {
+  const router = useRouter();
+
   return (
     <header className="fixed top-0 left-0 right-0 z-50 bg-transparent backdrop-blur-sm">
       <div className="container mx-auto px-4 py-4 flex items-center justify-between">
         <Link href="/" className="text-2xl font-bold text-white">
-          <Image src={`/andai.png`} alt='andai' width={65} height={65} className=' rounded-md' />
+          <Image
+            src={`/andai.png`}
+            alt="andai"
+            width={65}
+            height={65}
+            className=" rounded-md"
+          />
         </Link>
 
         <div className="hidden md:flex items-center space-x-8">
@@ -41,13 +50,17 @@ export default function Header() {
             {/* <Button variant="ghost" className="text-gray-300 hover:text-white">
               Login
             </Button> */}
-            <Button className="bg-gradient-to-r from-purple-600 to-pink-600 hover:from-purple-700 hover:to-pink-700 text-white border-0">
+            <Button
+              onClick={() => {
+                router.push("https://andai-marketplace.vercel.app/");
+              }}
+              className="bg-gradient-to-r from-purple-600 to-pink-600 hover:from-purple-700 hover:to-pink-700 text-white border-0"
+            >
               Get Started Today
             </Button>
           </div>
         </div>
       </div>
     </header>
-  )
+  );
 }
-
